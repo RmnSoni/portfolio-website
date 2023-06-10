525d7b704343e2aceae4c7b2b0d00d91a16dacae
gasher <69788442+RmnSoni@users.noreply.github.com>
2023-06-10 15:40:19 +0530

    Fixed Dwonload resume button

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,6 @@
 
 import React, { useRef } from 'react';
-
+import Resume from "../asset/RamanSoni_resumesde.pdf";
 import { FaEnvelope, FaGithub, FaLinkedin } from 'react-icons/fa';
 import { HiDocumentArrowDown } from 'react-icons/hi2';
 
@@ -46,11 +46,11 @@ function Home() {
 
         </ul>
       </div>
-      <div className='flex text-3xl pt-5 text-center justify-between'>
+      <div className='flex text-3xl sM pt-5 text-center justify-between'>
         <a target='_blank' className='m-2  hover:text-teal-600 flex-1 flex justify-center' href='https://www.linkedin.com/in/rmnsoni/'><FaLinkedin /></a>
         <a target='_blank' className='m-2 hover:text-teal-600 flex-1 flex justify-center' href='https://github.com/RmnSoni' ><FaGithub/></a>
         <a className='m-2 hover:text-teal-600 flex-1 flex justify-center' href='mailto:soni.raman.1602@gmail.com' >< FaEnvelope/></a>
-        <a className='m-2 hover:text-teal-600 flex-1 flex justify-center' href='../asset/RamanSoni_resumesde.pdf'download='placeholderResume.pdf'><HiDocumentArrowDown/></a>
+        <a className='m-2 hover:text-teal-600 flex-1 flex justify-center' href={Resume} download='placeholderResume.pdf'><HiDocumentArrowDown/></a>
       </div>
     </div>
 
