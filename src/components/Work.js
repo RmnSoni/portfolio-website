31e0db2ce6c08d3b20cc2d74e36994032f025270
gasher <69788442+RmnSoni@users.noreply.github.com>
2023-06-11 18:01:23 +0530

    work section group

--- src/components/Work.js
+++ src/components/Work.js
@@ -5,7 +5,8 @@ export default class Work extends Component {
         return (
             <div className='py-4 group/section'>
                 <h2 className='group-hover/section:text-teal-500 text-2xl ' > Work Experience</h2>
-                <div className='flex group' >
+                <div className='group'>
+                <div className='flex' >
                     <div className='text-slate-400 min-w-fit m-1 p-1'> Aug - Dec 2022</div>
                     <div className='m-1 p-1' >
                         <h3 className='text-teal-500 text-xl group-hover:font-semibold' >Management Trainee</h3>
@@ -20,7 +21,7 @@ export default class Work extends Component {
                 <ul className='sm:hidden'>
                     <li>Initiated and managed the development and implementation of a digital visiting card solution for over 125,000 ICICI employees, significantly reducing reliance on printed cards. This involved coordination with teams of business analyst, developers, designers.</li>
                     <li>In my role as the application owner, I undertook the responsibility of ensuring the maintenance, scalability, new enhancements, and technical support for iSpace—an indispensable application relied upon by a vast user base of over 125,000 banking professionals on a daily basis.</li>
-                </ul>
+                </ul></div>
             </div>
         )
     }
